1457055fb06c48e37832e8a85735d63cadd2bafb
Stekunda Henry <stekundah@gmail.com>
2023-03-23 11:36:43 -0400

    Added toggle functioniality between user login & registration

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 const fs = require("fs");
 
 const port = 3000;
@@ -22,4 +22,24 @@ server.listen(port, function (error) {
     } else {
         console.log("Server is listening on port " + port);
     }
+});
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document
+        .querySelector("#linkCreateAccount")
+        .addEventListener("click", (e) => {
+            e.preventDefault();
+            loginForm.classList.add("form--hidden");
+            createAccountForm.classList.remove("form--hidden");
+        });
+
+    document.querySelector("#linkLogin").addEventListener("click", (e) => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
 });
